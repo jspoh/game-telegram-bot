2675db8483b891eacf4e234208396fd04e48efde
jspoh <jingsengyeah@gmail.com>
2023-04-19 14:37:54 +0800

    updated python syntax to run on 3.9

--- api/index.py
+++ api/index.py
@@ -44,6 +44,8 @@ def getCauseway(camera: str):
         cwCctv.close_driver()
         return {'data': cwCctv.all_cameras()}
 
+    # rewrite match statement since vercel does not support 3.10 syntax
+    '''
     match camera:
         case 'wdls-to-jb':
             cwCctv.close_driver()
@@ -61,6 +63,24 @@ def getCauseway(camera: str):
             cwCctv.close_driver()
             return {'data': (make_response(
                 {'error': 'Bad request, {} does not exist'.format(camera)}, 400))}
+    '''
+
+    if camera == 'wdls-to-jb':
+        cwCctv.close_driver()
+        return {'data': (cwCctv.wdls_to_jb())}
+    elif camera == 'wdls-to-bke':
+        cwCctv.close_driver()
+        return {'data': (cwCctv.wdls_to_bke())}
+    elif camera == 'view-from-tuas':
+        cwCctv.close_driver()
+        return {'data': (cwCctv.view_from_tuas())}
+    elif camera == 'tuas-second-link':
+        cwCctv.close_driver()
+        return {'data': (cwCctv.tuas_second_link())}
+    else:
+        cwCctv.close_driver()
+        return {'data': (make_response(
+            {'error': 'Bad request, {} does not exist'.format(camera)}, 400))}
 
 
 @app.route('/datetime')
@@ -71,6 +91,7 @@ def getDatetime():
 @app.route('/game/<game>', defaults={'option': None}, methods=['GET'])
 @app.route('/game/<game>/<option>', methods=['GET'])
 def gameHandler(game: str, option: str):
+    '''
     match game:
         case 'tod' | 'truth-or-dare':
             match option:
@@ -86,6 +107,22 @@ def gameHandler(game: str, option: str):
             msg = requests.get(
                 'https://api.truthordarebot.xyz/api/nhie').json()['question']
             return {'data': (msg)}
+    '''
+
+    if game == 'tod' or game == 'truth-or-dare':
+        if option == 't' or option == 'truth':
+            truth = requests.get(
+                'https://api.truthordarebot.xyz/v1/truth').json()['question']
+            return {'data': (truth)}
+        elif option == 'd' or option == 'dare':
+            dare = requests.get(
+                'https://api.truthordarebot.xyz/api/dare').json()['question']
+            return {'data': (dare)}
+
+    elif game == 'nhie' or game == 'never-have-i-ever':
+        msg = requests.get(
+            'https://api.truthordarebot.xyz/api/nhie').json()['question']
+        return {'data': (msg)}
 
 
 if __name__ == '__main__':
